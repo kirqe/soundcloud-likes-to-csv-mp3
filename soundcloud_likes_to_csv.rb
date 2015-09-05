4900504f986095b995cb2a5e100c43bb4880f748
railsr <ikll7@yahoo.com>
2015-09-05 14:25:20 +0400

    edits

--- soundcloud_likes_to_csv.rb
+++ soundcloud_likes_to_csv.rb
@@ -6,12 +6,27 @@ require 'csv'
 CLIENT_ID = "YOUR_CLIENT_ID" # Client ID of created app
 USERNAME = "USERNAME" # your username
 
-client = SoundCloud.new(:client_id => CLIENT_ID)
-tracks = client.get("/users/#{USERNAME}/favorites", :order => 'created_at DESC')
 
-CSV.open('favorites.csv', "wb") do |csv|
-  csv << ["Title", "URL"]
-  tracks.each do |track|
-    csv << [track.title, track.permalink_url]
+page_size = 50
+client = SoundCloud.new(:client_id => CLIENT_ID)
+res = []
+tracks = client.get("/users/#{USERNAME}/favorites", limit: page_size, order: 'created_at', linked_partitioning: 1)
+
+tracks.collection.each do |track|
+  res << [track.title, track.permalink_url]
+end
+
+while tracks.next_href
+  tracks = client.get(tracks.next_href)
+  tracks.collection.each do |track|
+    res << [track.title, track.permalink_url]
   end
 end
+
+CSV.open("likes.csv", 'wb') do |csv|
+  res.each do |track|
+    csv << track
+  end
+end
+
+
