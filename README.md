0eb09af346871a53d55720b1b0a7e4ba002b9672
kirqe <ikll7@yahoo.com>
2017-08-14 00:42:57 +0300

    improvements + download likes as mp3 :+1: :+1:

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-Install soundcloud gem with `gem install soundcloud`
+Ruby script that allows to export soundcloud likes to csv or download liked tracks as mp3
 
-Add a new app at http://soundcloud.com/you/apps and get your CLIENT_ID
+## Instructions
 
-Replace CLIENT_ID and USERNAME in the .rb
+- Install the following gems
+  ```
+  gem install soundcloud
+  gem install whirly # shows progress in terminal
+  ```
 
-Some of the tracks might be missing probably due to some privacy setting of the user who uploaded them.
+Run the script with one of the following commands:
+
+```
+ruby soundcloud_likes_export.rb username # both csv and download likes as mp3
+
+ruby soundcloud_likes_export.rb username -csv # only save likes to csv
+
+ruby soundcloud_likes_export.rb username -dl # only download all likes as mp3
+```
+
+Files will be saved to the folder from which the script was started.
+
+*If CLIENT_ID key stops working, just replace it with a different one.*
+
+Some of the tracks might be missing probably due to some privacy setting of the user who uploaded them or api key restrictions.
+I noticed that some api keys allow access to certain songs and others return nil or empty data.
--- soundcloud_likes_to_csv.rb
+++ soundcloud_likes_to_csv.rb
@@ -1,28 +1,82 @@
 # add a new app at http://soundcloud.com/you/apps and get your CLIENT_ID
 
 require 'soundcloud'
+require 'open-uri'
 require 'csv'
+require 'whirly'
 
-CLIENT_ID = "YOUR_CLIENT_ID" # Client ID of created app
-USERNAME = "USERNAME" # your username
+CLIENT_ID = "a3e059563d7fd3372b49b37f00a00bcf" # Client ID of created app
+username = ARGV[0] # Your username
+
+return if username.nil?
 
 page_size = 200
-res = []
-client = SoundCloud.new(:client_id => CLIENT_ID)
-tracks = client.get("/users/#{USERNAME}/favorites", limit: page_size, order: 'created_at', linked_partitioning: 1)
+@res = []
+@client = SoundCloud.new(:client_id => CLIENT_ID)
+tracks = @client.get("/users/#{username}/favorites", limit: page_size, order: 'created_at', linked_partitioning: 1)
 
 loop do
   if tracks.next_href
-    res += tracks.collection
-    tracks = client.get(tracks.next_href)
+    @res += tracks.collection
+    tracks = @client.get(tracks.next_href)
   else
-    res += tracks.collection
+    @res += tracks.collection
     break
   end
 end
 
-CSV.open("likes.csv", 'wb') do |csv|
-  res.uniq.each do |track|
-    csv << [track.title, track.permalink_url]
+# save all tracks to csv likes.csv
+def export_csv(tracks)
+  CSV.open("likes.csv", 'wb') do |csv|
+    tracks.uniq.each do |track|
+      csv << [track.title, track.permalink_url]
+    end
   end
+end
+
+def format_name(name)
+  replacements = [ ["/", "-"], ["!", ""] ]
+  replacements.each { |r| name.gsub!(r[0], r[1]) }
+  name
+end
+
+def download_track(track_id, name)
+  Dir.mkdir("likes") unless Dir.exists?("likes")
+
+  track_path = "./likes/#{format_name(name)}.mp3"
+
+  unless File.exists?(track_path)
+    url = @client.get("/tracks/#{track_id}/streams").http_mp3_128_url
+    if url
+      File.open(track_path, "wb") do |mp3|
+        mp3 << open(url, 'rb').read
+      end
+    end
+  end
+  Whirly.status = "Downloaded #{name}"
+end
+
+# download all tracks to /tracks folder
+def batch_download
+  tracks = @res.uniq.map { |track| [ track.id, track.title ] }
+  threads = []
+  Whirly.start spinner: "dots", status: "Downloading..." do
+    tracks.each_slice(8) do |batch|
+      batch.each do |track|
+        threads << Thread.new { download_track(track[0], track[1]) }
+      end
+    end
+
+    threads.each { |thread| thread.join }
+  end
+end
+
+case
+  when ARGV.include?("-csv")
+    export_csv(@res)
+  when ARGV.include?("-dl")
+    batch_download
+  else
+    export_csv(@res)
+    batch_download
 end
